0fd816a152450c03d5945f5ac0580800dd3e566c
natthapon3090 <garfield11kp@gmail.com>
2026-02-20 00:01:22 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class League(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    league_id = db.Column(db.Integer, db.ForeignKey('league.id'))
 
