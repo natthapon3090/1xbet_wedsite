b76355763af9742ad61ff970ae8d40f2b0863eb1
natthapon3090 <garfield11kp@gmail.com>
2026-02-20 00:28:36 +0700

    Update models.py

--- models.py
+++ models.py
@@ -21,3 +21,8 @@ class Match(db.Model):
     odds_draw = db.Column(db.Float)
     odds_away = db.Column(db.Float)
     status = db.Column(db.String(20), default='open')
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(200))
+    balance = db.Column(db.Float, default=1000)
