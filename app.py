91926fe4e7870b248e5f871620d70b42e06745be
natthapon3090 <garfield11kp@gmail.com>
2026-02-20 20:31:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, League, Team, Match, User, BetSlip, BetItem
+import random
 
+app = Flask(__name__, template_folder='.', static_folder='.')
+app.config['SECRET_KEY'] = 'bet-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
