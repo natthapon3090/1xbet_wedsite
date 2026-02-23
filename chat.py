64043884a473bcb56fb2e9ed5cd59cdb1cd05bb4
natthapon3090 <garfield11kp@gmail.com>
2026-02-23 17:38:18 +0700

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,1 +1,28 @@
+from flask import Blueprint,render_template_string,session,request,redirect
+from database import db
+from layout import css,header
 
+chat_bp=Blueprint("chat",__name__)
+
+@chat_bp.route("/chat",methods=["GET","POST"])
+def chat():
+
+    if "user" not in session:
+        return redirect("/login")
+
+    con=db()
+
+    if request.method=="POST":
+
+        msg=request.form.get("msg","")
+
+        if msg!="":
+
+            con.execute(
+            "INSERT INTO chat(user,msg) VALUES(?,?)",
+            (session["user"],msg)
+            )
+
+            con.commit()
+
+            return redirect("/chat")
