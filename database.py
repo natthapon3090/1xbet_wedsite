b249638d9c2aa7b83d74c7c4fcaefb022d1d584e
natthapon3090 <garfield11kp@gmail.com>
2026-02-24 21:45:30 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,17 @@
+import sqlite3
+
+def db():
+    return sqlite3.connect("database.db")
+
+def init():
+
+    con=db()
+
+    con.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT,
+    money INTEGER)
+    """)
 
