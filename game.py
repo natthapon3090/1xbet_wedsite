42eaa7257294b15182c626168fa811e7e1f3f26f
natthapon3090 <garfield11kp@gmail.com>
2026-02-25 23:18:09 +0700

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
 
+from flask import Blueprint,render_template_string,session,redirect
+from database import db
+from layout import css,header
+import random
+
+game_bp=Blueprint("game",__name__)
+
+@game_bp.route("/game")
+def game():
+
+    if "user" not in session:
+        return redirect("/login")
+
+    return render_template_string(css()+header()+"""
+<h2>หมุนวงล้อ 10 บาท</h2>
+<a href='/spin'>
+<button>หมุน</button>
+</a>
+</div>
+""")
