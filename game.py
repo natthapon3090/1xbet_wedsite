95ae0efa9acbb78b2cf3b543c6995fa99867ef6f
natthapon3090 <garfield11kp@gmail.com>
2026-02-26 21:23:54 +0700

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-
 from flask import Blueprint,render_template_string,session,redirect
 from database import db
 from layout import css,header
@@ -18,4 +17,40 @@ def game():
 <button>หมุน</button>
 </a>
 </div>
+""")
+
+@game_bp.route("/spin")
+def spin():
+
+    con=db()
+
+    user=con.execute(
+    "SELECT * FROM users WHERE username=?",
+    (session["user"],)
+    ).fetchone()
+
+    if user[3]<10:
+        return redirect("/wallet")
+
+    reward=random.choice([0,5,10,20,50,100])
+
+    newmoney=user[3]-10+reward
+
+    con.execute(
+    "UPDATE users SET money=? WHERE username=?",
+    (newmoney,session["user"])
+    )
+
+    con.commit()
+
+    return render_template_string(css()+header()+f"""
+
+<h2>ได้ {reward} บาท</h2>
+
+<a href='/game'>
+<button>หมุนอีกครั้ง</button>
+</a>
+
+</div>
+
 """)
