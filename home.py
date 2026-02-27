a0600ef23d8f63eab12d95915f73b532cf13d413
natthapon3090 <garfield11kp@gmail.com>
2026-02-27 20:52:10 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,1 +1,17 @@
+from flask import Blueprint,render_template_string
+from layout import css,header
 
+home_bp=Blueprint("home",__name__)
+
+@home_bp.route("/")
+def home():
+
+    return render_template_string(css()+header()+"""
+
+<h2>หน้าแรก</h2>
+
+ยินดีต้อนรับร้านค้าออนไลน์
+
+</div>
+
+""")
